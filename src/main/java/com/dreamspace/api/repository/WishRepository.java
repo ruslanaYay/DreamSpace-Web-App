6325a726789d4c807cb7ea7f447065ba8359134e
Ruslana <ruslana.iuzyfovych@nure.ua>
2026-05-21 23:37:47 +0300

    DW-56 Отримати список всіх бажань конкретного вішліста

--- src/main/java/com/dreamspace/api/repository/WishRepository.java
+++ src/main/java/com/dreamspace/api/repository/WishRepository.java
@@ -5,6 +5,7 @@ import com.dreamspace.api.entity.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface WishRepository extends JpaRepository<Wish, Long> {
 
     // знаходить найперше бажання з картинкою у конкретному вішлісті
     Optional<Wish> findFirstByWishlistIdAndImageUrlIsNotNullOrderByCreatedAtAsc(Long wishlistId);
+    //отримує бажання, пов'язані з вішлістом, і сортує їх
+    List<Wish> findAllByWishlist_IdOrderByCreatedAtDesc(Long wishlistId);
 }
